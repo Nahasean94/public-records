fd84b0becf275273b04db0aa82e0f285d136492f
Nahashon Njenga <nahasean94@hotmail.com>
2018-06-06 05:13:34 +0300

    Commit changes to push to GitHub

--- contracts/Ecde.sol
+++ contracts/Ecde.sol
@@ -1,6 +1,8 @@
 pragma solidity ^0.4.0;
 
 contract Ecde {
+    string
+    string institution;
     function Ecde(){
 
     }
---
+++ contracts/Institutions.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.4.0;
+
+contract Institutions {
+    struct details{
+
+    }
+    function Institutions(){
+
+    }
+}
